dab38f8b645a97441652dff193dd55dfd838673f
BlackFlash01 <30633381+blackflash01@users.noreply.github.com>
2018-05-24 16:07:10 +0200

    Update __resource.lua

--- riot2/__resource.lua
+++ riot2/__resource.lua
@@ -13,7 +13,7 @@ files {
 
 
 data_file 'CARCOLS_FILE' 'data/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+--data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
 data_file 'HANDLING_FILE' 'data/handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
 
